9a8a9977b282a83af6a29d5170bf432bb935e3d5
GingerNinja2962 <60173629+GingerNinja2962@users.noreply.github.com>
2020-06-27 15:42:12 +0200

    Create Interpreter.c
    Not yet finished, just writing down thoughts at this point

---
+++ Interpreter.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+void ft_execute(int *array, char c)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int c;
+    int *array;
+
+    array = malloc(size of(int) * 2048);
+
+    
+}
