6e6c8ff5a1fc4ed77cea6cd8e0ab2557c24006ae
GingerNinja2962 <60173629+GingerNinja2962@users.noreply.github.com>
2020-06-27 16:25:03 +0200

    Update Interpreter.c
    Just saving changes,

--- Interpreter.c
+++ Interpreter.c
@@ -1,9 +1,30 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-void ft_execute(int *array, char c)
+void ft_putchar(char c)
 {
+    write(1, &c, 1);
+}
 
+void 
+
+int ft_execute(int *array, char c, int j)
+{
+    if (c == '+')
+        array[j] = (array[j] + 1);
+    if (c == '-')
+        array[j] = (array[j] + 1);
+    if (c == '>')
+        j++;
+    if (c == '<')
+        j--;
+    if (c == '.')
+        ft_putchar(array[j]);
+    if (c == '[')
+        
+    if (c == ']')
+        
+    return (j);
 }
 
 int main(int argc, char *argv[])
@@ -14,5 +35,14 @@ int main(int argc, char *argv[])
 
     array = malloc(size of(int) * 2048);
 
-    
+    if (argc == 2)
+    {
+        while (argv[1][i])
+        {
+            ft_execute(array, argv[1][i]);
+            i++;
+        }
+    }
+    ft_putchar('\n');
+    return (0);
 }
